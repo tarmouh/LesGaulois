450475c849a9256d6de6fd71590a28dd3d7d0c24
mmouh <mmouh@tarmouh>
2022-10-18 17:08:57 +0200

    Fin du TP2
    - création de la méthode s'équiper
    
    - équipement du romain

--- src/personnages/Equipement.java
+++ src/personnages/Equipement.java
@@ -4,8 +4,7 @@ public enum Equipement {
 	CASQUE("casque"), BOUCLIER("bouclier");
 
 	private String nom;
-	private Equipement[] Equipements = new Equipement[2];
-	private int nbEquipement = 0;
+
 
 	private Equipement(String nom) {
 		this.nom = nom;
--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -3,12 +3,13 @@ package personnages;
 public class Romain {
 	private String nom;
 	private int force;
+	private Equipement[] equipements = new Equipement[2];
+	private int nbEquipement = 0;
 
 	public Romain(String nom, int force) {
 		this.nom = nom;
 		this.force = force;
-		
-		
+
 	}
 
 	public String getNom() {
@@ -31,23 +32,54 @@ public class Romain {
 			parler("J'abandonne...");
 		}
 	}
-	
-	public static boolean forceToujoursPositive(Romain romain) {
-		return romain.force>=0;
-		
-	}
-	
 
-	
+	public static boolean forceToujoursPositive(Romain romain) {
+		return romain.force >= 0;
+
+	}
+
+	private void ajouterAfficherEquipement(Equipement equipement, int indEquipement) {
+		equipements[indEquipement] = equipement;
+		nbEquipement += 1;
+		System.out.println("Le soldat " + nom + " s'équipe avec " + equipement + " !");
+	}
+
+	public void sEquiper(Equipement equipement) {
+		switch (nbEquipement) {
+		case 2:
+			System.out.println("Le soldat " + nom + " est déjà bien protégé !");
+			break;
+
+		case 1:
+			if (equipement.equals(equipements[0])) {
+				System.out.println("Le soldat " + nom + " possède déjà un " + equipement + " !");
+
+			} else {
+				ajouterAfficherEquipement(equipement, 1);
+			}
+			break;
+
+		default:
+			ajouterAfficherEquipement(equipement, 0);
+			break;
+		}
+	}
+
 	public static void main(String[] args) {
-		Romain Minus = new Romain("Minus",6);
-		int varForce = Minus.force;
-/* Precondition */		assert forceToujoursPositive(Minus);
-		Minus.recevoirCoup(1);
-/* Postcondition */		assert varForce < Minus.force;
+		Romain Minus = new Romain("Minus", 6);
+//		int varForce = Minus.force;
+		/* Precondition */ // assert forceToujoursPositive(Minus);
+//		Minus.recevoirCoup(1);
+		/* Postcondition */ // assert varForce < Minus.force;
 //		System.out.println(Equipement.BOUCLIER);
 //		System.out.println(Equipement.CASQUE);
-	
+
+		Equipement casque = Equipement.CASQUE;
+		Equipement bouclier = Equipement.BOUCLIER;
+		Minus.sEquiper(casque);
+		Minus.sEquiper(casque);
+		Minus.sEquiper(bouclier);
+		Minus.sEquiper(bouclier);
 	}
 
 }
